e5cd1ffd08a8582dcfe648343ad764d0c77d75de
esraelif <elifesratunca@gmail.com>
2024-08-26 10:53:18 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+// import Component from "./starter/01-return";
+import Component from "./starter/02-props";
+function App() {
+  return (
+    <main>
+      <Component />
+    </main>
+  );
+}
+
+export default App;
